afb673858a67a3d61e6cd2d7514e4fa45ef7063d
Hummel <chrisschulz@gmx.net>
2019-01-26 11:26:03 +0100

    bla2

--- NUnitTestProject1/UnitTest1.cs
+++ NUnitTestProject1/UnitTest1.cs
@@ -1,18 +1,22 @@
+using appveyor.netcore;
 using NUnit.Framework;
 
 namespace Tests
 {
     public class Tests
     {
+        private object _bla;
+
         [SetUp]
         public void Setup()
         {
+            _bla = new Class1();
         }
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            Assert.IsNotNull(_bla);
         }
     }
 }
--- appveyor.netcore/Class1.cs
+++ appveyor.netcore/Class1.cs
@@ -4,5 +4,9 @@ namespace appveyor.netcore
 {
     public class Class1
     {
+        public Class1()
+        {
+            Console.WriteLine();
+        }
     }
 }
